229bd67efb51678adf4d63bfbe290b7f589861af
Marcos Verblud <m.verblud@naranjax.com>
2023-03-08 18:02:17 -0300

    add validationSchema joi .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/configuration';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     MongooseModule.forRoot(process.env.MONGODB),
   ],
