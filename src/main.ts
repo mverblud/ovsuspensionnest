15a2275746c3efe2a525a498e547950ab1d41b6f
Marcos Verblud <m.verblud@naranjax.com>
2023-03-20 16:44:56 -0300

    add morgan and CORS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const logger = new Logger('Bootstrap');
 
-  app.setGlobalPrefix('api');
+  app.use(morgan('dev'));
 
+  const CORS: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  };
+
+  app.enableCors(CORS);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,7 +26,7 @@ async function bootstrap() {
       transform: true,
       transformOptions: {
         enableImplicitConversion: true,
-      }
+      },
     }),
   );
 
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      MONGODB: string;
+      DEFAULT_LIMIT: Number
+    }
+  }
